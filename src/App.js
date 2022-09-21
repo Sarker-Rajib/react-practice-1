b3738fccd50c3d9c6ce3d118e065e23c1a6078e2
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-09-21 18:26:42 +0600

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello</h2>
+      <Counter></Counter>
+      <LoadComments></LoadComments>
     </div>
   );
 }
+
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount(count - 1);
+  return(
+    <div>
+      <h1>Counter : {count}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+    </div>
+  )
+}
+
+
+function LoadComments() {
+  const [comments, setComments] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => setComments(data))
+  }, [])
+
+
+  return(
+    <div>
+      {
+        comments.map(comment => <Comment email={comment.email}></Comment>)
+      }
+    </div>
+  )
+}
+
+
+function Comment(props) {
+  return (
+    <div className="">
+      <h2>{props.email}</h2>
+    </div>
+  );
+}
+
 
 export default App;
